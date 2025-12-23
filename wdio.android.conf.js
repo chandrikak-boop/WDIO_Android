08e971b767634f73f3d48c77ea0a92da4350fc9b
chandrikak-boop <chandrikak@testvagrant.com>
2025-12-23 12:27:13 +0530

    browser stack update

--- wdio.android.conf.js
+++ wdio.android.conf.js
@@ -10,12 +10,28 @@ import {config} from './wdio.shared.conf.js';
         ]
     config.capabilities = [ {
         // capabilities for local Appium web tests on an Android Emulator
-        'appium:platformName': process.env.PLATFORM_NAME,
-        'appium:deviceName': process.env.DEVICE_NAME,
-       'appium:platformVersion': process.env.PLATFORM_VERSION,
+        'appium:platformName': "android",
+        'appium:deviceName': "Google Pixel 7",
+       'appium:platformVersion': "13.0",
         'appium:automationName': 'UiAutomator2',
-        'appium:app': process.env.APP_PATH,
+        'appium:app': "app/android/ApiDemos-debug.apk",
         "appium:autoGrantPermissions": true
     }]
+       //
+    // Test runner services
+    // Services take over a specific job you don't want to take care of. They enhance
+    // your test setup with almost no effort. Unlike plugins, they don't add new
+    // commands. Instead, they hook themselves up into the test process.
+
+   config.services= [
+  ['appium', {
+    command: 'appium',
+    args: {
+      port: 4723,
+      relaxedSecurity: true,
+      sessionOverride: true
+    }
+  }]
+]
 
 exports.config = config;
--- wdio.shared.conf.js
+++ wdio.shared.conf.js
@@ -7,8 +7,8 @@ export const config = {
     // Runner Configuration
     // ====================
     // WebdriverIO supports running e2e tests as well as unit and component tests.
-    runner: 'local',
-    port: 4723,
+    // runner: 'local',
+    // port: 4723,
     //
     // ==================
     // Specify Test Files
@@ -97,12 +97,7 @@ export const config = {
     //
     // Default request retries count
     connectionRetryCount: 3,
-    //
-    // Test runner services
-    // Services take over a specific job you don't want to take care of. They enhance
-    // your test setup with almost no effort. Unlike plugins, they don't add new
-    // commands. Instead, they hook themselves up into the test process.
-    //services: ['appium'],
+   
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
